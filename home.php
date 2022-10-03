53c50f31ce65912d78c62cb5b883c63decb616ab
Jacob Jabakumar.S <76088012+jackhack2002@users.noreply.github.com>
2022-10-03 20:01:07 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,64 @@
+<?php 
+require './vendor/autoload.php';
+
+$redis = new Predis\Client();
+$redis->connect('127.0.0.1', 6379);
+
+
+$cache_key = md5($sql);
+
+            if ($redis->exists($cache_key)) {
+
+                $data_source = "Data from Redis Server";
+                $data = unserialize($redis->get($cache_key));
+
+            } else {
+
+                $data_source = 'Data from MySQL Database';
+
+                $db_name     = 'guvi';
+                $unmae  = "root";
+                $db_password = '';
+                $db_host     = 'localhost:3306';
+
+                $pdo = new PDO('mysql:host=' . $db_host . '; dbname=' . $db_name, $db_user, $db_password);
+                $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+                $stmt = $pdo->prepare($sql);
+                $stmt->execute();
+                $data = []; 
+
+                while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {          
+                   $data[] = $row;  
+                }  
+
+                $redis->set($cache_key, serialize($data)); 
+                $redis->expire($cache_key, 86,400);        
+           }
+
+session_start();
+
+if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
+
+ ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HOME</title>
+     <link rel="icon" href="img/websitelogo.png">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+     <h1>Hello, <?php echo $_SESSION['name']; ?></h1>
+     <h1>Your Email is, <?php echo $_SESSION['email']; ?></h1>
+     <h1>Your DOB is, <?php echo $_SESSION['dob']; ?></h1>
+     <a href="logout.php">Logout</a>
+</body>
+</html>
+
+<?php 
+}else{
+     header("Location: index.php");
+     exit();
+}
+ ?>
